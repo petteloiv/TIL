e1156da254e9794049a1e9532df360c279ec4d0f
petteloiv <petteloiv@gmail.com>
2022-01-15 22:50:29 +0900

    0115

--- Review_Problems/0114_review.py
+++ Review_Problems/0114_review.py
@@ -9,11 +9,12 @@ name = input('도시의 이름을 영어로 입력해주세요 : ')
 url = f'https://www.metaweather.com/api/location/search/?query={name}'
 response = requests.get(url).json()
 
-woeid = int(response["woeid"])
+woeid = response[0]['woeid'] #api의 리스트 안의 딕셔너리의 요소... 키값 
 
 url2 = f'https://www.metaweather.com/api/location/{woeid}'
 response2 = requests.get(url2).json()
 
-weather = str(response["weather_state_name"])
+consolidated = list((response2.keys())[0]['weather_state_name'])
+weather = consolidated[1]['weather_state_name']
 
 print(f"{name}의 날씨는 {weather}입니다.")
